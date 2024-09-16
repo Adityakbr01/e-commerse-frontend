7becc01880da570249adb80e5c586f27da710963
Adityakbr01 <aditykbr01@gmail.com>
2024-09-16 18:51:29 +0530

    ""

--- vite.config.js
+++ vite.config.js
@@ -12,7 +12,7 @@ export default defineConfig({
   build: {
     outDir: "dist", // Default output directory
     rollupOptions: {
-      external: ["react-toastify"],
+      // external: ["react-toastify"],
     },
   },
 });
